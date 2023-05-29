62f50412179bc6daa7a21b2ac9cf2f40f3bfa0b4
DenisS <semenovdenis3201@gmail.com>
2023-05-29 20:25:54 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static int Zadanie1(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int minElement = matrix[0, 0];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] < minElement)
+                    {
+                        minElement = matrix[i, j];
+                    }
+                }
+            }
+
+            return minElement;
+        }
+
+        public static int Zadanie2(int[,] matrix, int row)
+        {
+            int cols = matrix.GetLength(1);
+            int sum = 0;
+
+            for (int j = 0; j < cols; j++)
+            {
+                if (matrix[row, j] > 0)
+                {
+                    sum += matrix[row, j];
+                }
+            }
+
+            return sum;
+        }
+
+        public static int[] Zadanie3(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int[] zeroCounts = new int[cols];
+
+            for (int j = 0; j < cols; j++)
+            {
+                int count = 0;
+                for (int i = 0; i < rows; i++)
+                {
+                    if (matrix[i, j] == 0)
+                    {
+                        count++;
+                    }
+                }
+                zeroCounts[j] = count;
+            }
+
+            return zeroCounts;
+        }
+
+        public static int[] Zadanie4(int[,] matrix, int row)
+        {
+            int cols = matrix.GetLength(1);
+            int sum = Zadanie2(matrix, row);
+            int[] elements = new int[cols];
+            int count = 0;
+
+            for (int j = 0; j < cols; j++)
+            {
+                if (matrix[row, j] > sum)
+                {
+                    elements[count] = matrix[row, j];
+                    count++;
+                }
+            }
+
+            Array.Resize(ref elements, count);
+
+            return elements;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,151 @@
+﻿using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1Zadanie1()
+        {
+            int[,] matrix = { { 5, 8, 3 },{ 2, 9, 7 }, { 1, 4, 6 } };
+            int expected = 1;
+            int actual = Class1.Zadanie1(matrix);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod2Zadanie1()
+        {
+            int[,] matrix = { { 7, 5, 2 }, { 11, 3, 4 }, { 14, 1, 12 } };
+            int expected = 1;
+            int actual = Class1.Zadanie1(matrix);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod3Zadanie1()
+        {
+            int[,] matrix = {{ 87, 45, 15 }, { 11, 22, 40 }, { 55, 77, 5 } };
+            int expected = 5;
+            int actual = Class1.Zadanie1(matrix);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod4Zadanie1()
+        {
+            int[,] matrix = { { 55, 68, 73 }, { 82, 49, 17 }, {51, 44, 36 } };
+            int expected = 17;
+            int actual = Class1.Zadanie1(matrix);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod5Zadanie1()
+        {
+            int[,] matrix = { { 55, 84, 4 },{ 78, 54, 73 }, { 5, 7, 61} };
+            int expected = 4;
+            int actual = Class1.Zadanie1(matrix);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod1Zadanie2()
+        {
+            int[,] matrix = { { -1, 2, 3 },{ 4, -5, 6 }, { -7, 8, -9 } };
+            int row = 1;
+            int expected = 10;
+            int actual = Class1.Zadanie2(matrix, row);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2Zadanie2()
+        {
+            int[,] matrix = { { 1, 1, 1, 1 }, { 2, 2, 2, 2 }, { 3, 3, 3, 3 }, { 4, 4, 4, 4 } };
+            int row = 3;
+            int expected = 16;
+            int actual = Class1.Zadanie2(matrix, row);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod3Zadanie2()
+        {
+            int[,] matrix = { { 1, 2, 3 },{ 4, 5, 6 },{ 7, 8, 9 } };
+            int row = 1;
+            int expected = 15;
+            int actual = Class1.Zadanie2(matrix, row);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void TestMethod4Zadanie2()
+        {
+            int[,] matrix = { { 1, 2, 3 },  { 4, 5, 6 },{ 7, 8, 9 } };
+            int row = 1;
+            int expected = 15;
+            int actual = Class1.Zadanie2(matrix, row);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod5Zadanie2()
+        {
+            int[,] matrix = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+            int row = 1;
+            int expected = 0;
+            int actual = Class1.Zadanie2(matrix, row);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void TestMethod1Zadanie3()
+        {
+            int[,] matrix = { { 0, 1, 0 }, { 2, 0, 3 }, { 0, 4, 5 }
+        };
+            int[] expected = { 2, 1, 1 };
+            int[] actual = Class1.Zadanie3(matrix);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2Zadanie3()
+        {
+            int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[] expected = { 0, 0, 0 };
+            int[] actual = Class1.Zadanie3(matrix);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod3Zadanie3()
+        {
+            int[,] matrix = { { 1, 2, 0 }, { 4, 0, 6 }, { 0, 8, 9 } };
+            int[] expected = { 1, 1, 1 };
+            int[] actual = Class1.Zadanie3(matrix);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod4Zadanie3()
+        {//на пустую матрицу тест
+            int[,] matrix = new int[0, 0];
+            int[] expected = new int[0];
+            int[] actual = Class1.Zadanie3(matrix);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod5Zadanie3()
+        {// нулевыми строками:
+            int[,] matrix = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+            int[] expected = { 3, 3, 3 };
+            int[] actual = Class1.Zadanie3(matrix);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
+
+
